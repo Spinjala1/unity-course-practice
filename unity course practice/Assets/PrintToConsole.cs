fa0b2c66050817e4fcc19987bb3b92819e70afaf
Spinjala1 <154847740+Spinjala1@users.noreply.github.com>
2023-12-30 19:34:58 -0800

    unity basic course finished

--- unity course practice/Assets/PrintToConsole.cs
+++ unity course practice/Assets/PrintToConsole.cs
@@ -7,7 +7,7 @@ public class PrintToConsole : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        Debug.Log("I,m printing to the console");
+        Debug.Log("Hello I have no doors or windows who on earth designed me");
     }
 
     // Update is called once per frame
